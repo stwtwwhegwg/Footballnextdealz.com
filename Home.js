5b1e1423d77c6129c801ede6b9797bd57a32a3c9
stwtwwhegwg <abdelashalaby7@gmail.com>
2025-06-01 14:00:07 +0200

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Willkommen bei FootballNextDealz</h1>
+      <p>
+        Deine neue Anlaufstelle für Fußball-News, Transfers, Quiz und Spiele.
+      </p>
+      <p>
+        Bleib dran, die App und Website werden bald verfügbar sein!
+      </p>
+    </div>
+  );
+}
